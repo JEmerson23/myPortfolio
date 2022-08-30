c5274ae534bbbf8b9e09ce9a32385f78b7faf93c
Emerson23 <92601129+JEmerson23@users.noreply.github.com>
2022-08-29 21:50:59 -0300

    update index.js

--- scriptsJS/index.js
+++ scriptsJS/index.js
@@ -1,3 +1,7 @@
+const rememberLater = {
+contentCheckedButton: null
+};
+
 const contentButton = document.getElementsByName("content");
 
 const myPresentation = document.getElementById("my_presentation"),
@@ -15,19 +19,19 @@ function changeContent() {
     if (element.checked) {
       switch (element.value) {
         case "0":
-          console.log(0);
+          localStorage.setItem("contentCheckedButton","0");
           myPresentation.style.display = "flex";
           myProjects.style.display = "none";
           myContacts.style.display = "none";
           break;
         case "1":
-          console.log(1);
+          localStorage.setItem("contentCheckedButton","1");
           myPresentation.style.display = "none";
           myProjects.style.display = "flex";
           myContacts.style.display = "none";
           break;
         case "2":
-          console.log(2);
+          localStorage.setItem("contentCheckedButton","2");
           myPresentation.style.display = "none";
           myProjects.style.display = "none";
           myContacts.style.display = "flex";
@@ -36,3 +40,13 @@ function changeContent() {
     }
   });
 }
+
+window.onload = function(){
+  if(!localStorage.contentCheckedButton){
+   localStorage.setItem("contentCheckedButton","0");
+  } else {
+    rememberLater.contentCheckedButton = localStorage.getItem("contentCheckedButton");
+  }
+  contentButton[rememberLater.contentCheckedButton-0].checked = true;
+  changeContent();
+};
