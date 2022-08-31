9f1f47236a61496dcddd0fa0516faa7c65f6483e
Emerson23 <92601129+JEmerson23@users.noreply.github.com>
2022-08-30 23:04:22 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,10 @@
 import * as indexPage from "/scripts/singlePage.js";
-
-const rememberLater = {
-contentCheckedButton: null
-};
+import * as load from "/scripts/loadContent.js";
 
 window.onload = function(){
-  if(!localStorage.contentCheckedButton){
-   localStorage.setItem("contentCheckedButton","0");
-  } else {
-    rememberLater.contentCheckedButton = localStorage.getItem("contentCheckedButton");
-  }
-  indexPage.contentButton[rememberLater.contentCheckedButton-0].checked = true;
+  indexPage.contentButton[indexPage.rememberPage()].checked = true;
+  
   indexPage.changeContent();
+  
+  load.loadContacts()
 };
