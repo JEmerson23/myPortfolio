1632e8c9a08576575ec41991fde5faa9960e6ee6
Emerson23 <92601129+JEmerson23@users.noreply.github.com>
2022-09-02 22:21:50 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,33 @@
 import * as indexPage from "/scripts/singlePage.js";
-import * as load from "/scripts/loadContent.js";
+
+import * as content from "/scripts/loadContent.js";
 
 window.onload = function(){
   indexPage.contentButton[indexPage.rememberPage()].checked = true;
   
   indexPage.changeContent();
   
-  load.loadContacts()
+  content.loadContacts()
 };
+
+const mainHeader = document.getElementById("main_header");
+
+function fixedHeader(){
+  let {scrollY,innerWidth} = window;
+  if(innerWidth <= 768){
+    if(scrollY >= 1)
+     mainHeader.style.cssText = 'position:fixed;top:0;z-index:1;';
+    else if(scrollY == 0)
+     mainHeader.style.cssText = 'position:relative;';
+  } else {
+     mainHeader.style.cssText = 'position:relative;';
+  }
+}
+ 
+window.onscroll = function(){
+  fixedHeader();
+};
+
+window.onresize = function(){
+  fixedHeader();
+}
